e0938fd0e65c2d6e8ed4bb1c9668e70e09a70eb0
Bérenger Thévenet <YiffyC@users.noreply.github.com>
2018-03-13 10:51:59 +0100

    TP0
    Lecture d'un webservice

---
+++ TP0/src/fr/gkj/tp0/Main.java
@@ -0,0 +1,20 @@
+package fr.gkj.tp0;
+
+import NET.webserviceX.www.LengthUnitSoap;
+import NET.webserviceX.www.LengthUnitSoapProxy;
+import NET.webserviceX.www.Lengths;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception
+	{
+		//LengthUnitSoap <-- interface
+		LengthUnitSoap service = new LengthUnitSoapProxy();
+		
+		double resultat = service.changeLengthUnit(1, Lengths.Feet, Lengths.Centimeters);
+		System.out.println(resultat);
+		
+		
+	}
+
+}
