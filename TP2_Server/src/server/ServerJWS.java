95603021ee80844ffd4b495bde0027f14803392b
Bérenger Thévenet <berenger.thevenet@laposte.net>
2018-03-16 14:40:26 +0100

    UP

--- TP2_Server/src/server/ServerJWS.java
+++ TP2_Server/src/server/ServerJWS.java
@@ -10,8 +10,8 @@ public class ServerJWS
 	public static void main(String[] args)
 	{
 
-		String url = "http://localhost:8585/";
-		//voir wsdl http://localhost:8585/BanqueWS?wsdl
+		String url = "http://localhost:8080/";
+		//voir wsdl http://localhost:8080/BanqueWS?wsdl
 		Endpoint.publish(url,  new BanqueService());
 		System.out.println(url);
 
