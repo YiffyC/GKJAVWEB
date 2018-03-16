c1fa35a4a36cf6fcc6160bc4e89ff4819642d418
Bérenger Thévenet <YiffyC@users.noreply.github.com>
2018-03-16 16:40:20 +0100

    (:

--- Rest-Server/src/fr/gkj/rest/services/BanqueService.java
+++ Rest-Server/src/fr/gkj/rest/services/BanqueService.java
@@ -4,7 +4,10 @@ import java.util.Date;
 import java.util.List;
 
 import javax.websocket.server.PathParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import metier.Compte;
 
@@ -12,19 +15,25 @@ import metier.Compte;
 @Path("/banque")
 public class BanqueService
 {
+	@GET
 	@Path("/conversion/{montant}")
 	public double conversion(@PathParam("montant") double mt)
 	{
 		return mt*1.11;
 	}
 	
+	@GET
 	@Path("/{code}")
+	@Produces({MediaType.APPLICATION_ATOM_XML, MediaType.APPLICATION_JSON})
 	public Compte getCompte(@PathParam("code")long code)
 	{
 		return new Compte(code, 7000, new Date());
 	}
 	
 
+	@GET
+	@Path("/comptes")
+	@Produces({MediaType.APPLICATION_ATOM_XML, MediaType.APPLICATION_JSON})
 	public List<Compte> getComptes()
 	{
 		List<Compte> cpts = new ArrayList<Compte>();
